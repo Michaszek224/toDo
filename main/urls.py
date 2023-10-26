9938002f79e6dea1cdf1a09a3ab3062d99d170b4
Michaszek <punkomichal@gmail.com>
2023-10-26 02:18:42 +0200

    Added completed status using js and ajax

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', homeView, name='homeView'),
+    path('complete/<int:task_id>/', completeTask, name='complete_task'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from .models import Task
 from .forms import TaskForm
-# Create your views here.
 
 def homeView(request):
     tasks = Task.objects.all()
@@ -13,7 +13,14 @@ def homeView(request):
 
         return redirect('homeView')
 
+    context = {'tasks': tasks}
+    return render(request, 'main/home.html', context)
 
-    context = {'tasks':tasks}
-    return render(request, 'main/home.html/', context)
+def completeTask(request, task_id):
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        task = Task.objects.get(pk=task_id)
+        task.isDone = not task.isDone  # Toggle the isDone field
+        task.save()
+        return JsonResponse({'message': 'Task status toggled successfully.'})
 
+    return redirect('homeView')
